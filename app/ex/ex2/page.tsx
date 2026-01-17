f0921106ec36e6475b6b302da41fb17c6b58d778
Rimaki <txwfyxa@gmail.com>
2026-01-17 21:45:10 +0700

    Update page.tsx

--- app/ex/ex2/page.tsx
+++ app/ex/ex2/page.tsx
@@ -1,1 +1,16 @@
+import React from "react";
 
+export default function Ex1Page() {
+    return (
+        <html>
+            <head>
+                <title>
+                    3.1
+                </title>
+            </head>
+            <body>
+                Unknown
+            </body>
+        </html>
+    )
+}
